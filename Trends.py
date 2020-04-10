6fe974f2e12d911c7bd44cdea8a5738659a6b76b
James Walker <walker.james39@hotmail.co.uk>
2020-04-10 10:39:15 +0100

    update search to variable

--- Trends.py
+++ Trends.py
@@ -8,7 +8,7 @@ from matplotlib import style
 pytrends = TrendReq(hl='en-US', tz=360)
 
 #key term
-kw = "Accountancy"
+kw = input("Please enter a google search: " )
 
 #payload
 kw_list = [kw]
@@ -16,9 +16,9 @@ pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='US')
 
 #Search for trend and return df
 IBR_df = pytrends.interest_by_region(resolution='REGION', inc_low_vol=False, inc_geo_code=False)
-IBR_df2 = IBR_df[ IBR_df['Accountancy'] != 0 ]
+IBR_df2 = IBR_df[ IBR_df[kw] != 0 ]
 IBR_df2.reset_index(inplace=True)
-IBR_df2.sort_values(by="Accountancy", ascending=False, inplace=True)
+IBR_df2.sort_values(by=kw, ascending=False, inplace=True)
 # IBR_df2["geoName"] = IBR_df2["geoName"].str.slice(0,-3)
 IBR_df2.head()
 
@@ -26,9 +26,9 @@ fig = plt.figure()
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 2, colspan=1)
 
 x = IBR_df2['geoName'].head(10)
-y = IBR_df2['Accountancy'].head(10)
+y = IBR_df2[kw].head(10)
 ax1.bar(x,y,color ='#8B0000')
-ax1.set_xticklabels(x, rotation=90, font)
+ax1.set_xticklabels(x, rotation=90)
 
 plt.title('Top 10 States Searching for '+ kw)
 plt.ylabel('Rel. Prop Searches')
@@ -43,7 +43,7 @@ IOT_df = pytrends.interest_over_time()
 IOT_df.drop(columns='isPartial', inplace=True)
 IOT_df.head()
 x = IOT_df.index
-y = IOT_df['Accountancy']
+y = IOT_df[kw]
 
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 2, colspan=1)
 plt.style.use('ggplot')
